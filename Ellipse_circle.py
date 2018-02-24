173dd398148c59e88caf62fa151dbfe61f4486be
Ankit28P <36223074+Ankit28P@users.noreply.github.com>
2018-02-23 23:06:58 -0800

    Add files via upload

---
+++ Ellipse_circle.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 08 22:33:58 2018
+
+@author: Ankit Patel
+"""
+
+
+import math
+
+class Ellipse:
+    def __init__(self, x0, y0, x, y):
+        self.x0 = x0
+        self.y0 = y0
+        self.x = x
+        self.y =y
+        
+    def area(self):
+        x,y = self.x,self.y
+        return math.pi*x*y
+    
+    def circumference(self):
+        x,y = self.x, self.y
+        return math.pi*(3*(x+y)-math.sqrt((3*x+y)*(x+3*y)))
+    
+class circle(Ellipse):
+    def __init__(self, x0, y0, r):
+        Ellipse.__init__(self, x0, y0, r, r)
+        
+C = circle(4,6,2)
+print C.area()
+print C.circumference()
